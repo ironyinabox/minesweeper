b709d6b176f4bd20ed8db1bb07cc10e19a7e0158
Ariel & Jason <ariel+jason@aA.io>
2015-08-03 13:42:40 -0400

    added render, hardcoded BOMB_NUM

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Tile
   attr_accessor :is_bomb, :status, :neighbors
 
@@ -23,21 +25,41 @@ class Tile
     is_bomb
   end
 
+  def to_s
+    is_bomb? ? "*" : "_"
+  end
+
 end
 
 class Board
   BOARD_DIM = 9
-  BOMBS_NUM = BOARD_DIM
+  BOMBS_NUM = 10
 
   attr_accessor :board
 
   def initialize
-    board = Array.new(BOARD_DIM ** 2, Tile.new(false))
-    BOMBS_NUM.times { |i| board[i].is_bomb? = true }
+    board = Array.new(BOARD_DIM ** 2) {Tile.new(false)}
+    # debugger
+    BOMBS_NUM.times { |i| board[i].is_bomb = true }
     board.shuffle!
-    @board = board.each_slice(BOARD_DIM)
+    @board = board.each_slice(BOARD_DIM).to_a
   end
+
+  def render
+    board.each do |row|
+      row.each do |tile|
+        print tile.to_s
+      end
+      puts "\n"
+    end
+  end
+
 end
 
 class Game
+end
+
+if __FILE__ == $PROGRAM_NAME
+  b = Board.new
+  b.render
 end
