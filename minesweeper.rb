d91947ab9e980431a7c240dee32fab3531c3f63e
Ariel & Jason <ariel+jason@aA.io>
2015-08-03 12:18:59 -0400

    Create Board class, initialize

--- minesweeper.rb
+++ minesweeper.rb
@@ -8,4 +8,21 @@ class Tile
     @status = :hidden
     @neighbors = []
   end
+  
+end
+
+class Board
+  BOARD_DIM = 9
+  BOMBS_NUM = BOARD_DIM
+
+  attr_accessor :board
+
+  def initialize
+    board = Array.new(BOARD_DIM ** 2, Tile.new(false))
+    BOMBS_NUM.times do |i|
+      board[i].is_bomb = true
+    end
+    board.shuffle!
+    @board = board.each_slice(BOARD_DIM)
+  end
 end
